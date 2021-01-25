58e23208a7973d84870e2f6acf1cd1f2257724f7
Caroline <carolinefallonlucas@gmail.com>
2021-01-25 15:24:08 -0500

    server fixes

---
+++ api/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const carsRouter = require('./carsRouter');
+
+const server = express();
+ 
+
+server.use(express.json()); 
+
+server.use('/cars', carsRouter); 
+
+
+
+module.exports = server; 
--- index.js
+++ index.js
@@ -1,18 +1,9 @@
-// require the express npm module, needs to be added to the project using "npm install express"
-const express = require('express');
 
-// creates an express application using the express module
-const server = express();
 
-// configures our server to execute a function for every GET request to "/"
-// the second argument passed to the .get() method is the "Route Handler Function"
-// the route handler function will run on every GET request to "/"
-server.get('/', (req, res) => {
-  // express will pass the request and response objects to this function
-  // the .send() on the response object can be used to send a response to the client
-  res.send('Hello World');
+
+const server = require("./api/server");
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => {
+  console.log(`** LISTENING ON PORT ${port} **`);
 });
-
-// once the server is fully configured we can have it "listen" for connections on a particular "port"
-// the callback function passed as the second argument will run once when the server starts
-server.listen(8000, () => console.log('API running on port 8000'));
